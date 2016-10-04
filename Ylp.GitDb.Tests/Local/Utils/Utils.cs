de3ce21c2aebe4eb331ae42f86467d149f8fb561
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-04 18:18:25 +0200

    Add minimal set of tests to test implementation of the local repository

---
+++ Ylp.GitDb.Tests/Local/Utils/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ylp.GitDb.Tests.Local.Utils
+{
+    class Utils
+    {
+        public static T Catch<T>(Action action) where T : Exception
+        {
+            try
+            {
+                action();
+                return null;
+            }
+            catch (Exception ex)
+            {
+                var exception = ex as T;
+                if (exception != null)
+                    return exception;
+                throw;
+            }
+        }
+    }
+}
---
+++ Ylp.GitDb.Tests/Local/Utils/WithRepo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Castle.Core.Internal;
+using GitTest.Core;
+using LibGit2Sharp;
+using Moq.AutoMock;
+using Ylp.GitDb.Core.Interfaces;
+using Ylp.GitDb.Local;
+
+namespace Ylp.GitDb.Tests.Local.Utils
+{
+    public class WithRepo : IDisposable
+    {
+        protected IGitDb Subject;
+        protected readonly string LocalPath = Path.GetTempPath() + Guid.NewGuid();
+        protected readonly Repository Repo;
+        public WithRepo()
+        {
+            Subject = new LocalGitDb(LocalPath, new AutoMocker().Get<ILogger>());
+            Repo = new Repository(LocalPath);
+        }
+
+        static void deleteReadOnlyDirectory(string directory)
+        {
+            Directory.EnumerateDirectories(directory)
+                     .ForEach(deleteReadOnlyDirectory);
+            Directory.EnumerateFiles(directory).Select(file => new FileInfo(file) { Attributes = FileAttributes.Normal })
+                    .ForEach(fi => fi.Delete());
+            Directory.Delete(directory);
+        }
+        public void Dispose()
+        {
+            Subject.Dispose();
+            Repo.Dispose();
+            deleteReadOnlyDirectory(LocalPath);
+        }
+    }
+}
