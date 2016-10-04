a57b8706a10895c4aa985a60e30991e8d0939027
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-04 18:17:36 +0200

    Create core interfaces to implement in implementations

---
+++ Ylp.GitDb.Core/ILogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GitTest.Core
+{
+    public interface ILogger
+    {
+        Task Log(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public readonly string FileName;
+
+        public Logger(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public Task Log(string message)
+        {
+            File.AppendAllText(FileName, $"{DateTime.Now.ToString("HH:mm:ss")}: {message}\n");
+            return Task.CompletedTask;
+        }
+    }
+}
