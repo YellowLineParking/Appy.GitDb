98c700dbe3391f901f3f4bbf6f3c6033c2a380ac
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-04 18:17:36 +0200

    Create core interfaces to implement in implementations

---
+++ Ylp.GitDb.Core/Interfaces/IGitDb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ylp.GitDb.Core.Model;
+
+namespace Ylp.GitDb.Core.Interfaces
+{
+    public interface IGitDb : IDisposable
+    {
+        Task<string> Get(string branch, string key);
+        Task<T> Get<T>(string branch, string key) where T : class;
+
+        Task<IReadOnlyCollection<T>> GetFiles<T>(string branch, string key);
+        Task<IReadOnlyCollection<string>> GetFiles(string branch, string key);
+
+        Task<string> Save(string branch, string message, Document document, Author author);
+        Task<string> Save<T>(string branch, string message, Document<T> document, Author author);
+
+        Task<string> Delete(string branch, string key, string message, Author author);
+
+        Task Tag(Reference reference);
+        Task CreateBranch(Reference reference);
+        Task<IEnumerable<string>> GetAllBranches();
+        ITransaction CreateTransaction(string branch);
+    }
+}
---
+++ Ylp.GitDb.Core/Model/Reference.cs
@@ -0,0 +1,8 @@
+namespace Ylp.GitDb.Core.Model
+{
+    public class Reference
+    {
+        public string Pointer { get; set; }
+        public string Name { get; set; }
+    }
+}
