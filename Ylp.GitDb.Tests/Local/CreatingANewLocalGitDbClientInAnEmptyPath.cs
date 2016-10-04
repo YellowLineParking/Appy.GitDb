63b75cfe6f68d589b23a479ef1700e98fd5690f5
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-04 18:18:25 +0200

    Add minimal set of tests to test implementation of the local repository

---
+++ Ylp.GitDb.Tests/Local/CreatingANewLocalGitDbClientInAnEmptyPath.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+using Ylp.GitDb.Tests.Local.Utils;
+
+namespace Ylp.GitDb.Tests.Local
+{
+    public class CreatingANewLocalGitDbClientInAnEmptyPath : WithRepo
+    {
+        [Fact]
+        public void InitializesTheRepository() =>
+            Directory.Exists($@"{LocalPath}\.git").Should().BeTrue();
+
+        [Fact]
+        public void AddsAMasterBranch() =>
+            Repo.Branches.Select(b => b.FriendlyName).Should().Contain("master");
+
+        [Fact]
+        public void CreatesAnInitialCommit() =>
+            Repo.Branches["master"].Commits.Count().Should().Be(1);
+    }
+}
