e89809ee531cfd6d1cae4c9a0b897621690ad867
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-07 16:37:23 +0200

    Added an API endpoint to the Git Server to access a repository over HTTP

---
+++ Ylp.GitDb.Core/Model/ApiModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Ylp.GitDb.Core.Model
+{
+    public class SaveRequest
+    {
+        public string Message { get; set; }
+        public Document Document { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class DeleteRequest
+    {
+        public string Message { get; set; }
+        public string Key { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class CommitTransaction
+    {
+        public string Message { get; set; }
+        public Author Author { get; set; }
+    }
+}
--- Ylp.GitDb.Server/Program.cs
+++ Ylp.GitDb.Server/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using Ylp.GitDb.Core;
+using Ylp.GitDb.Local;
 
 namespace Ylp.GitDb.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var url = ConfigurationManager.AppSettings["server.url"];
+            var serverLog = ConfigurationManager.AppSettings["server.log"];
+            var gitRepoPath = ConfigurationManager.AppSettings["git.repository.path"];
+            var gitLog = ConfigurationManager.AppSettings["git.log"];
+            var app = App.Create(url, new LocalGitDb(gitRepoPath, new Logger(gitLog)), new Logger(serverLog));
+            using (app.Start())
+            {
+                Console.WriteLine($"Server started on {url}");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            };
         }
     }
 }
