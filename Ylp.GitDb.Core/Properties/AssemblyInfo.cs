9adc3e70c8231022ee72bace520acfbb650c6366
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-07 17:03:06 +0200

    Version the packages

--- Ylp.GitDb.Core/Properties/AssemblyInfo.cs
+++ Ylp.GitDb.Core/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("72ae8930-98ef-446d-8167-7dd251bcf4e6")]
 
-[assembly: AssemblyVersion("0.0.2")]
-[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
