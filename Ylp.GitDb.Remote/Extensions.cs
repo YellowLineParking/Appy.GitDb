a1395d78925fbd5646000209fe546b645821ef7a
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-07 16:39:17 +0200

    Added a client that implements IGitDb to access the remote endpoint

---
+++ Ylp.GitDb.Remote/Extensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GitTest.RemoteGitDb
+{
+    static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> items, int maxItems)
+        {
+            return items.Select((item, inx) => new { item, inx })
+                        .GroupBy(x => x.inx / maxItems)
+                        .Select(g => g.Select(x => x.item));
+        }
+
+        public static async Task<T> GetAsync<T>(this HttpClient client, string url) where T : class
+        {
+            try
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var result = await (await client.GetAsync(url)).Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(result);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+            
+
+        public static Task<HttpResponseMessage> PostAsync<T>(this HttpClient client, string url, T item) =>
+            client.PostAsync(url, new StringContent(JsonConvert.SerializeObject(item), Encoding.UTF8, "application/json"));
+
+        public static async Task<T> JsonResponse<T>(this Task<HttpResponseMessage> task) =>
+            JsonConvert.DeserializeObject<T>(await (await task).Content.ReadAsStringAsync());
+
+        public static async Task<string> AsStringResponse(this Task<HttpResponseMessage> task) =>
+            await (await task).Content.ReadAsStringAsync();
+
+        public static async Task<HttpResponseMessage> WhenSuccessful(this Task<HttpResponseMessage> task)
+        {
+            var result = await task;
+            if (result.StatusCode != HttpStatusCode.OK) throw new Exception(await result.Content.ReadAsStringAsync());
+            return result;
+        }
+    }
+}
---
+++ Ylp.GitDb.Remote/RemoteTransaction.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using GitTest.RemoteGitDb;
+using Ylp.GitDb.Core.Interfaces;
+using Ylp.GitDb.Core.Model;
+
+namespace Ylp.GitDb.Remote
+{
+    class RemoteTransaction : ITransaction
+    {
+        readonly HttpClient _client;
+        readonly string _baseUrl;
+        readonly string _transactionId;
+        bool _isOpen = false;
+        public RemoteTransaction(HttpClient client, string baseUrl, string branch)
+        {
+            _client = client;
+            _baseUrl = baseUrl;
+            _transactionId = _client.PostAsync(url($"/{branch}/transaction"), new StringContent("", Encoding.UTF8))
+                                    .WhenSuccessful()
+                                    .Result
+                                    .Content
+                                    .ReadAsStringAsync()
+                                    .Result
+                                    .Replace("\"", "");
+            _isOpen = true;
+        }
+
+        T executeIfOpen<T>(Func<T> action)
+        {
+            if (_isOpen)
+                return action();
+            throw new Exception("Transaction is not open");
+        }
+
+        string url(string resource) => _baseUrl + resource;
+
+        public Task Add(Document document) =>
+            executeIfOpen(() => _client.PostAsync(url($"/{_transactionId}/add"), document).WhenSuccessful());
+
+        public Task Add<T>(Document<T> document) => 
+            Add(Document.From(document));
+
+        public Task Delete(string key) =>
+            executeIfOpen(() => _client.PostAsync(url($"/{_transactionId}/delete/{key}"), new StringContent("", Encoding.UTF8)));
+
+        public Task DeleteMany(IEnumerable<string> keys) =>
+            executeIfOpen(async () =>
+             {
+                 foreach (var batch in keys.Batch(50))
+                     await _client.PostAsync(url($"/{_transactionId}/deleteMany"), batch);
+             });
+            
+
+        public Task AddMany<T>(IEnumerable<Document<T>> documents) =>
+            AddMany(documents.AsParallel().Select(Document.From));
+
+        public Task AddMany(IEnumerable<Document> documents) =>
+            executeIfOpen(async () =>
+            {
+                foreach (var batch in documents.Batch(50))
+                    await _client.PostAsync(url($"/{_transactionId}/addMany"), batch);
+            });
+
+        public async Task<string> Commit(string message, Author author)
+        {
+            if (!_isOpen)
+                throw new Exception("Transaction is not open");
+
+            _isOpen = false;
+            return await _client.PostAsync(url($"/{_transactionId}/commit"), new CommitTransaction
+            {
+                Message = message,
+                Author = author
+            }).WhenSuccessful()
+              .AsStringResponse();
+        }
+
+        public async Task Abort()
+        {
+            if (_isOpen)
+                await _client.PostAsync(url($"/{_transactionId}/abort"), new StringContent("", Encoding.UTF8));
+            _isOpen = false;
+        }
+
+        public void Dispose() =>
+            Abort().Wait();
+    }
+}
