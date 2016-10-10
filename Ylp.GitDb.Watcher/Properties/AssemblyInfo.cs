b5f0ed63b64d79ae48e593323f30e07f14f6fb29
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-10 12:28:04 +0200

    Added project to monitor a repository and send events so we can hook up the repository for data indexing

---
+++ Ylp.GitDb.Watcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ylp.GitDb.Watcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Yellow Line Parking")]
+[assembly: AssemblyProduct("Ylp.GitDb.Watcher")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3eba271-650e-4482-bdea-41f569051b4d")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
