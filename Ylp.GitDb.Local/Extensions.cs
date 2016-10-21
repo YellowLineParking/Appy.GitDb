b9e43df6b81c7152225c80d8258ca3b39ad5376d
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-21 14:35:49 +0200

    Don't do a full diff when committing, instead just compare the hashes because this is a lot faster

--- Ylp.GitDb.Local/Extensions.cs
+++ Ylp.GitDb.Local/Extensions.cs
@@ -6,18 +6,6 @@ namespace Ylp.GitDb.Local
 {
     internal static class Extensions
     {
-        public static bool HasChanges(this Repository repository, Tree tree1, Tree tree2)
-        {
-            var result = repository.Diff.Compare<TreeChanges>(tree1, tree2);
-            return result.Added.Any() || 
-                   result.Conflicted.Any() || 
-                   result.Copied.Any() || 
-                   result.Deleted.Any() || 
-                   result.Modified.Any() || 
-                   result.Renamed.Any() || 
-                   result.TypeChanged.Any();
-        }
-
         public static T As<T>(this string source) =>
             JsonConvert.DeserializeObject<T>(source);
     }
