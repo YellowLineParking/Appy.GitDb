51913273094dd113c49b4ac3920aa3b859da4ed0
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-10-21 19:03:01 +0200

    Add a benchmark project

---
+++ Ylp.GitDb.Benchmark/Scripts/bundle_repo.ps1
@@ -0,0 +1,8 @@
+﻿param([string] $gitFolder,
+	  [string] $outputPath)
+
+$output = [System.IO.Path]::GetFullPath($outputPath)
+pushd $gitFolder
+git bundle create $output --branches --tags 2>&1
+popd
+Remove-Item -Recurse -Force $gitFolder
---
+++ Ylp.GitDb.Benchmark/Scripts/convert_bare_repo.ps1
@@ -0,0 +1,16 @@
+﻿param([string]$gitFolder)
+
+pushd $gitFolder
+
+#move all folder into a .git subdirectory
+mkdir .git
+Get-ChildItem -Exclude '.git' -Recurse | Move-Item -Destination {Join-Path .git $_.Name}
+
+#initialize the repo
+git init
+
+#set the HEAD to the first commit so we don't have a dirty working directory
+$firstCommitHash = git rev-list --max-parents=0 HEAD
+git checkout --detach $firstCommitHash
+
+popd
