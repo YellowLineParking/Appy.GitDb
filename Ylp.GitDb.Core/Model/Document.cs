efaa20e303d584a5869bbd617ccc4f8f5823d6d9
Kenneth Truyers <truyers.kenneth@gmail.com>
2016-12-13 14:31:39 +0100

    Save documents indented for easier merging

--- Ylp.GitDb.Core/Model/Document.cs
+++ Ylp.GitDb.Core/Model/Document.cs
@@ -5,7 +5,7 @@ namespace Ylp.GitDb.Core.Model
     public class Document : Document<string>
     {
         public static Document From<T>(Document<T> source) =>
-            new Document {Key = source.Key, Value = JsonConvert.SerializeObject(source.Value)};
+            new Document {Key = source.Key, Value = JsonConvert.SerializeObject(source.Value, Formatting.Indented)};
     }
     public class Document<T>
     {
