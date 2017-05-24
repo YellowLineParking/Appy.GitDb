5436c90e8f8d84fe9d52b182165fa20a9963cdff
Kenneth Truyers <truyers.kenneth@gmail.com>
2017-05-24 19:40:44 +0200

    Make the watcher raise an event for a new branch with all modifications, even if it can't find a base branch

--- Ylp.GitDb.Tests/Watcher/Branching.cs
+++ Ylp.GitDb.Tests/Watcher/Branching.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
 using Ylp.GitDb.Core.Model;
@@ -7,7 +9,7 @@ using Ylp.GitDb.Watcher;
 
 namespace Ylp.GitDb.Tests.Watcher
 {
-    public class AddingABranch : WithWatcher
+    public class AddingABranchAtTheSameCommitAsAnotherBranch : WithWatcher
     {
         protected override Task Because() =>
             GitDb.CreateBranch(new Reference {Name = "test", Pointer = "master"});
@@ -18,6 +20,27 @@ namespace Ylp.GitDb.Tests.Watcher
                    .WithArgs<BranchAdded>(args => args.BaseBranch == "master" &&
                                                   args.Branch.Name == "test" && 
                                                   args.Branch.Commit == Repo.Branches["master"].Tip.Sha);
+    }
+
+    public class AddingABranchAtACommitFromADifferentBranch : WithWatcher
+    {
+        string _pointer;
+
+        protected override Task Because()
+        {
+            var rnd = new Random().Next(0, Repo.Branches["master"].Commits.Count());
+            _pointer = Repo.Branches["master"].Commits.Skip(rnd).First().Sha;
+            return GitDb.CreateBranch(new Reference { Name = "test", Pointer = _pointer });
+        }
+            
+
+        [Fact]
+        public void RaisesABranchAddedEvent() =>
+            Subject.ShouldRaise("BranchAdded")
+                   .WithArgs<BranchAdded>(args => args.BaseBranch == "master" &&
+                                                  args.Branch.Name == "test" &&
+                                                  args.Branch.Commit == _pointer &&
+                                                  args.Deleted.Any());
     }
 
     public class RemovingABranch : WithWatcher
