5b746ca41fea0435ae9d6ef6d64ef2a5546246d1
Jorge Rodriguez Galan <jrogalan@gmail.com>
2019-12-12 11:33:51 +0000

    Convert Appy.GitDb.Local project to sdk style with Appy.GitDb.Core included as dll

--- Appy.GitDb.Local/Properties/AssemblyInfo.cs
+++ Appy.GitDb.Local/Properties/AssemblyInfo.cs
@@ -1,17 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Appy.GitDb.Local")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("AppyParking")]
-[assembly: AssemblyProduct("Appy.GitDb.Local")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-
 [assembly: Guid("37d87fb6-e7d6-4217-b021-38cea7233e6f")]
 
-[assembly: AssemblyVersion("0.16.2")]
